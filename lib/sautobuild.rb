b2b0a141b38cef29c6f08952321df914b5433e23
Patrick J Cherry <patrick@bytemark.co.uk>
2016-10-11 22:24:51 +0100

    Rubocop tidy

--- lib/sautobuild.rb
+++ lib/sautobuild.rb
@@ -7,26 +7,26 @@ require 'getoptlong'
 require 'zlib'
 
 class Sautobuild
-
-
   attr_reader :build_dir, :source_dir, :sources_list, :apt_conf, :apt_key
   attr_writer :source, :update_chroot, :builder_flags
 
   def initialize(dir)
-    raise "No build directory specified" if dir.to_s.empty?
-    raise Errno::ENOENT, dir unless File.exist?(dir)
-    raise Errno::ENOTDIR, dir unless File.directory?(dir)
+    fail 'No build directory specified' if dir.to_s.empty?
+    fail Errno::ENOENT, dir unless File.exist?(dir)
+    fail Errno::ENOTDIR, dir unless File.directory?(dir)
 
     @source_dir = File.expand_path(dir)
-    @build_dir = File.expand_path(File.join(@source_dir, ".."))
+    @build_dir = File.expand_path(File.join(@source_dir, '..'))
     @update_chroot = false
     @version, @source, @distribution, @architecture = nil
     @architectures = @available_architectures = @available_distributions = @opts = @builder_flags = []
-    @available_architectures_by_distribution = Hash.new{|h,k| h[k] = []}
+    @available_architectures_by_distribution = Hash.new { |h, k| h[k] = [] }
     @sources_list = @apt_conf = @apt_key = nil
   end
 
-  def changelog; File.join(@source_dir, "debian", "changelog"); end
+  def changelog
+    File.join(@source_dir, 'debian', 'changelog')
+  end
 
   def distribution
     do_read_changelog if @distribution.nil?
@@ -34,7 +34,7 @@ class Sautobuild
   end
 
   def distribution=(d)
-    raise ArgumentError, "Unknown distribution #{d}" unless self.available_distributions.include?(d)
+    fail ArgumentError, "Unknown distribution #{d}" unless available_distributions.include?(d)
     @distribution = d
   end
 
@@ -49,69 +49,71 @@ class Sautobuild
   end
 
   def version=(v)
-    @version = v.gsub(/^[0-9]*:/,'')
+    @version = v.gsub(/^[0-9]*:/, '')
   end
 
   def architecture
     do_read_dsc if @architecture.nil?
     @architecture
   end
-  
-  def architecture=(a); @architecture = a; end
+
+  attr_writer :architecture
 
   def architectures
     do_read_dsc if @architecture.nil?
     return [] if @architecture.nil?
 
-    @architectures = @architecture.split(" ") if @architectures.empty?
+    @architectures = @architecture.split(' ') if @architectures.empty?
     @architectures
   end
 
   def architectures=(archs)
     archs.each do |a|
-      if self.available_architectures.include?(a) or %w(all any).include?(a)
+      if available_architectures.include?(a) || %w(all any).include?(a)
         @architectures << a
       else
         warn "Unknown architecture #{a}"
       end
     end
 
-    raise ArgumentError, "No valid/available architectures found in #{as.inspect}"
+    fail ArgumentError, "No valid/available architectures found in #{as.inspect}"
 
     @architectures
   end
 
   def sautobuild_opts=(a)
-    @opts=a
+    @opts = a
   end
 
   def sources_list=(f)
     f = File.expand_path(f)
-    raise Errno::ENOENT, f unless File.exists?(f)
+    fail Errno::ENOENT, f unless File.exist?(f)
     @update_chroot = false
     @sources_list = f
   end
 
   def apt_conf=(f)
     f = File.expand_path(f)
-    raise Errno::ENOENT, f unless File.exists?(f)
+    fail Errno::ENOENT, f unless File.exist?(f)
     @update_chroot = false
     @apt_conf = f
   end
 
-  def dsc; File.join(@build_dir,source+"_"+version+".dsc") ; end
+  def dsc
+    File.join(@build_dir, source + '_' + version + '.dsc')
+  end
 
   def build_architectures
     build_archs = []
 
-    self.architectures.each do |a|
+    architectures.each do |a|
       case a
-      when "any"
-        build_archs += self.available_architectures
-      when "all"
+      when 'any'
+        build_archs += available_architectures
+      when 'all'
         build_archs << `dpkg-architecture -qDEB_BUILD_ARCH_CPU`.chomp
       else
-        build_archs << a if self.available_architectures.include?(a)
+        build_archs << a if available_architectures.include?(a)
       end
     end
 
@@ -125,7 +127,7 @@ class Sautobuild
 
   def available_architectures
     do_find_distributions_and_architectures if @available_architectures.empty?
-    @available_architectures 
+    @available_architectures
   end
 
   def available_architectures_by_distribution
@@ -147,73 +149,77 @@ class Sautobuild
   end
 
   def check(exit_if_fail = false)
-    puts "="*80+"\n"
-    changes = (%w(source)+build_architectures).collect do |arch|
-      File.join(@build_dir,@source+"_"+@version+"_"+arch+".changes")
+    puts '=' * 80 + "\n"
+    changes = (%w(source) + build_architectures).collect do |arch|
+      File.join(@build_dir, @source + '_' + @version + '_' + arch + '.changes')
     end
 
-    if !system("schroot -c #{self.distribution} -- lintian -X cpy -I #{changes.join(" ")}") and exit_if_fail
-      exit $?.exitstatus
+    if !system("schroot -c #{distribution} -- lintian -X cpy -I #{changes.join(' ')}") && exit_if_fail
+      exit $CHILD_STATUS.exitstatus
     end
   end
 
-  def make_repo(args={})
-    args[:hashes] = do_make_sources_and_packages_files()
+  def make_repo(args = {})
+    args[:hashes] = do_make_sources_and_packages_files
     do_make_releases_file(args)
   end
 
   private
 
-  def do_or_die(c); puts "="*80+"\n"+c ; exit $?.exitstatus unless system(c); end
-  
-  def do_read_fields_from_io(io)
-    raise ArgumentError, "Not an IO object" unless io.is_a?(IO)
+  def do_or_die(c)
+    puts '=' * 80 + "\n" + c
+    exit $CHILD_STATUS.exitstatus unless system(c)
+  end
 
-    while (l = io.gets) do
+  def do_read_fields_from_io(io)
+    fail ArgumentError, 'Not an IO object' unless io.is_a?(IO)
+
+    while (l = io.gets)
       next unless l.chomp =~ /([^:]+): (.+)/
-      field = $1.downcase
-      value = $2
+      field = Regexp.last_match(1).downcase
+      value = Regexp.last_match(2)
       #
       # Deal with 1.8 and 1.9 instance variables
       #
       ivar = "@#{field}"
       ivar = ivar.to_sym unless /^1\.8/ =~ RUBY_VERSION
 
-      next unless self.instance_variables.include?(ivar) and self.instance_variable_get(ivar).nil?
-      next unless self.respond_to?(field+"=")
-      self.__send__(field+"=",value)
+      next unless instance_variables.include?(ivar) && instance_variable_get(ivar).nil?
+      next unless respond_to?(field + '=')
+      __send__(field + '=', value)
     end
   end
 
   def do_read_changelog
-    raise Errno::ENOENT, self.changelog unless File.file?(self.changelog)
+    fail Errno::ENOENT, changelog unless File.file?(changelog)
 
-    IO.popen("dpkg-parsechangelog -l#{self.changelog}") do |io| 
+    IO.popen("dpkg-parsechangelog -l#{changelog}") do |io|
       do_read_fields_from_io(io)
     end
   end
 
   def do_read_dsc
-    raise Errno::ENOENT, self.dsc unless File.file?(self.dsc)
+    fail Errno::ENOENT, dsc unless File.file?(dsc)
 
-    File.open(self.dsc) do |fh|
+    File.open(dsc) do |fh|
       do_read_fields_from_io(fh)
     end
   end
 
   def do_find_distributions_and_architectures
     valid_architectures = []
-    IO.popen("dpkg-architecture -L") do |io|
-      valid_architectures = io.readlines.collect{|l| l.chomp}
+    IO.popen('dpkg-architecture -L') do |io|
+      valid_architectures = io.readlines.collect(&:chomp)
     end
 
     archs = []
     dists = []
 
-    IO.popen("schroot -l") do |io|
-     io.readlines.each do |l|
+    IO.popen('schroot -l') do |io|
+      io.readlines.each do |l|
         next unless l =~ /^(?:(?:chroot|source):)?([^-]+)-(.*)$/
-        dist, arch = [$1, $2]
+        dist = Regexp.last_match(1)
+        arch = Regexp.last_match(2)
         next unless valid_architectures.include?(arch)
         dists << dist unless dists.include?(dist)
         archs << arch unless archs.include?(arch)
@@ -224,26 +230,26 @@ class Sautobuild
   end
 
   def do_build_with_gbp
-  	# Check to see if we're on a tagged commit.
+    # Check to see if we're on a tagged commit.
     tag = `git tag --contains`
 
-  	# If we're not do a "snapshot" build.
+    # If we're not do a "snapshot" build.
     if tag.empty?
-      do_or_die("gbp dch --ignore-branch -S -a") 
-      @builder_flags << "--git-ignore-new"
+      do_or_die('gbp dch --ignore-branch -S -a')
+      @builder_flags << '--git-ignore-new'
     end
 
     # Remove flags that we don't want to be run twice.
-    gbp_sautobuild_opts = @opts - [ "-G", "-k", "-s" ]
-    gbp_sautobuild_opts.unshift("-n") unless gbp_sautobuild_opts.include?("-n")
+    gbp_sautobuild_opts = @opts - ['-G', '-k', '-s']
+    gbp_sautobuild_opts.unshift('-n') unless gbp_sautobuild_opts.include?('-n')
 
     cmd = %w(gbp buildpackage)
-    cmd += @builder_flags 
-    cmd << "--git-builder=#{$0}"
-    cmd += gbp_sautobuild_opts 
+    cmd += @builder_flags
+    cmd << '--git-builder={$PROGRAM_NAME}'
+    cmd += gbp_sautobuild_opts
     cmd << @source_dir
 
-    do_or_die(cmd.join(" "))
+    do_or_die(cmd.join(' '))
   end
 
   def do_build_source
@@ -253,42 +259,40 @@ class Sautobuild
   end
 
   def do_build_debs
-    # 
+    #
     # Only need to build all arches once.
     #
-    built_all = false 
+    built_all = false
 
-    self.build_architectures.each do |arch|
-      cmd = ["cd #{@build_dir} && sbuild --arch=#{arch} --verbose --dist=#{self.distribution}"]
-      if !built_all
-        cmd << "--arch-all" 
+    build_architectures.each do |arch|
+      cmd = ["cd #{@build_dir} && sbuild --arch=#{arch} --verbose --dist=#{distribution}"]
+      unless built_all
+        cmd << '--arch-all'
         built_all = true
       end
 
-      if @builder_flags
-        cmd += @builder_flags
-      end
+      cmd += @builder_flags if @builder_flags
 
-      if self.apt_conf or self.sources_list
-        cmd << "--no-apt-update"
-        cmd << "--no-apt-upgrade" if @update_chroot
+      if apt_conf || sources_list
+        cmd << '--no-apt-update'
+        cmd << '--no-apt-upgrade' if @update_chroot
 
-        if self.apt_conf
-          cmd << "--chroot-setup-commands='sudo cp #{self.apt_conf} /etc/apt/apt.conf.d/'"
+        if apt_conf
+          cmd << "--chroot-setup-commands='sudo cp #{apt_conf} /etc/apt/apt.conf.d/'"
         end
 
-        if self.sources_list
-          cmd << "--chroot-setup-commands='sudo cp #{self.sources_list} /etc/apt/sources.list.d/'" 
+        if sources_list
+          cmd << "--chroot-setup-commands='sudo cp #{sources_list} /etc/apt/sources.list.d/'"
         end
 
         cmd << "--chroot-setup-commands='sudo apt-get update'"
         cmd << "--chroot-setup-commands='sudo apt-get upgrade'" if @update_chroot
       elsif @update_chroot
-        cmd << "--apt-upgrade"
+        cmd << '--apt-upgrade'
       end
 
-      cmd << self.dsc
-      do_or_die(cmd.join(" "))
+      cmd << dsc
+      do_or_die(cmd.join(' '))
     end
   end
 
@@ -296,11 +300,11 @@ class Sautobuild
     do_or_die("cd #{@build_dir} && dpkg-scansources . /dev/null > Sources.new")
     do_or_die("cd #{@build_dir} && dpkg-scanpackages -m . /dev/null > Packages.new")
 
-    hashes = Hash.new{|h,k| h[k] = Array.new}
+    hashes = Hash.new { |h, k| h[k] = [] }
 
     %w(Sources Packages).each do |f|
-      target = File.join(@build_dir,f)
-      FileUtils.mv("#{target}.new", target, :force => true)
+      target = File.join(@build_dir, f)
+      FileUtils.mv("#{target}.new", target, force: true)
       Zlib::GzipWriter.open("#{target}.gz") do |gz|
         gz.write File.read(target)
       end
@@ -308,39 +312,37 @@ class Sautobuild
       #
       # Generate the hashes
       [Digest::MD5, Digest::SHA1, Digest::SHA256].each do |hash|
-        ["",".gz"].each do |e|
-          hashes[hash.to_s.split(":").last] << ["",hash.hexdigest(File.read(target+e)), File.stat(target+e).size, File.basename(target+e)]
+        ['', '.gz'].each do |e|
+          hashes[hash.to_s.split(':').last] << ['', hash.hexdigest(File.read(target + e)), File.stat(target + e).size, File.basename(target + e)]
         end
       end
     end
 
-    return hashes
+    hashes
   end
 
   def do_make_releases_file(args)
     #
     # Now write the release file
     #
-    release =<<EOF
-Archive: #{self.distribution}
-Label: #{args[:label] || "Unknown"}
-Origin: #{args[:origin] || "Unknown"}
-Architectures: #{self.build_architectures} source
-Components: #{args[:components] || "main"}
+    release = <<EOF
+Archive: #{distribution}
+Label: #{args[:label] || 'Unknown'}
+Origin: #{args[:origin] || 'Unknown'}
+Architectures: #{build_architectures} source
+Components: #{args[:components] || 'main'}
 EOF
     #
     # Add the hashes.
     #
-    release << "MD5Sum: \n"+args[:hashes]["MD5"].collect{|m| m.join(" ")}.join("\n")+"\n"
-    release << "SHA1: \n" + args[:hashes]["SHA1"].collect{|m| m.join(" ")}.join("\n")+"\n"
-    release << "SHA256: \n" + args[:hashes]["SHA256"].collect{|m| m.join(" ")}.join("\n")+"\n"
-    
+    release << "MD5Sum: \n" + args[:hashes]['MD5'].collect { |m| m.join(' ') }.join("\n") + "\n"
+    release << "SHA1: \n" + args[:hashes]['SHA1'].collect { |m| m.join(' ') }.join("\n") + "\n"
+    release << "SHA256: \n" + args[:hashes]['SHA256'].collect { |m| m.join(' ') }.join("\n") + "\n"
+
     #
     # And write.
-    target = File.join(@build_dir,"Release")
-    File.open(target+".new","w+"){|fh| fh.puts(release)}
-    FileUtils.mv("#{target}.new", target, :force => true)
+    target = File.join(@build_dir, 'Release')
+    File.open(target + '.new', 'w+') { |fh| fh.puts(release) }
+    FileUtils.mv("#{target}.new", target, force: true)
   end
-
 end
-
